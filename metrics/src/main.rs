98ba81b02656c5a8b1816190af3ec06461977919
Cijo Thomas <cijo.thomas@gmail.com>
2024-04-23 13:49:13 -0700

    demo app to print metric on demand

--- metrics/src/main.rs
+++ metrics/src/main.rs
@@ -1,5 +1,5 @@
 use std::thread;
-
+use crossterm::event::{self, Event, KeyCode};
 use metrics::counter::Counter;
 
 fn main() {
@@ -8,7 +8,8 @@ fn main() {
     let attributes_in_diff_order = [("key1", "value1"), ("key2", "value2"), ("key3", "value3")];
     let attributes_in_diff_order2 = [("key1", "value1"), ("key3", "value3"), ("key2", "value2")];
 
-    while (true)
+    println!("Press 'Enter' to display metrics, 'Esc'/Ctrl+C to quit");
+    loop
     {
         counter.add("counter", &attributes);
         counter.add("counter", &attributes_in_diff_order);
@@ -18,7 +19,17 @@ fn main() {
 
         counter.add("counter2", &vec![]);
         counter.add("counter2", &vec![]);
-        thread::sleep(std::time::Duration::from_secs(1));
-        counter.display_metrics();
+        thread::sleep(std::time::Duration::from_secs(1));        
+
+        if event::poll(std::time::Duration::from_secs(0)).unwrap() {
+            match event::read().unwrap() {
+                Event::Key(event) => match event.code {
+                    KeyCode::Esc => break, // Quit the loop
+                    KeyCode::Enter => counter.display_metrics(), // Display metrics
+                    _ => {}
+                },
+                _ => {}
+            }
+        }
     }
 }
